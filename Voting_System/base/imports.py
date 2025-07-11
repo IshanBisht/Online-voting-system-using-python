fa741cc1a31d101ee0acfc7d14afe6d6f45f8e21
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-07-12 01:29:52 +0530

    Added: new imports

--- Voting_System/base/imports.py
+++ Voting_System/base/imports.py
@@ -1,9 +1,8 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QComboBox, QCheckBox
 from PyQt5.QtGui import QIcon, QFont , QPixmap, QPalette, QBrush
-from PyQt5.QtCore import Qt, QRect, QSize, QCoreApplication
+from PyQt5.QtCore import Qt, QRect, QSize, QCoreApplication, QSize
 
-import pymysql 
-#import mai
+import pymysql
 
 from typing import Final
 from sys import argv , exit
