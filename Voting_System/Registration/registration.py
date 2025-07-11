cec1764158797942c58c7ee04df09f0142b1a4b6
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-07-12 01:33:24 +0530

    Optimized: faster rendering

--- Voting_System/Registration/registration.py
+++ Voting_System/Registration/registration.py
@@ -1,4 +1,6 @@
 from Voting_System.base.base import *
+from .candidate_registration import *
+from .voter_registration import *
 
 
 class RegistrationWindow(QWidget):
@@ -8,28 +10,37 @@ class RegistrationWindow(QWidget):
     BUTTON_TITLE_CANDIDATE_REGISTRATION = "Candidate Registration"
     BUTTON_TITLE_VOTER_REGISTRATION     = "Voter Registration"
 
+
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle(TITLE_REGISTRATION_PAGE)
         self.setWindowIcon( ovs_app_config.getIcon() )
-        self.setStyleSheet(f"background-color: {COLOR_CODE_BACKGROUND};")
+        self.setStyleSheet( CSS_STYLE_FOR_WIDGETS )
         self.setFixedSize(self.FIXED_WIDTH, self.FIXED_HEIGHT)
         self.center_window()
 
+        self.candidate_registration = CandidateRegistration()
+        self.voter_registration = VoterRegistration()
+
         self.setup_ui()
 
+
+
     def center_window(self):
-        screen = self.screen().availableGeometry()
+        screen = ovs_app_config.getScreen()
         x = (screen.width() - self.FIXED_WIDTH) // 2
         y = (screen.height() - self.FIXED_HEIGHT) // 2
         self.setGeometry(QRect(x, y, self.FIXED_WIDTH, self.FIXED_HEIGHT))
 
+
+
     def setup_ui(self):
         layout = QVBoxLayout()
-        layout.setAlignment(Qt.AlignCenter)
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         heading = QLabel(TITLE_REGISTRATION_PAGE)
-        heading.setAlignment(Qt.AlignCenter)
+        heading.setAlignment(Qt.AlignmentFlag.AlignCenter)
         heading.setFont( ovs_app_config.getHeadingFont() )
         heading.setStyleSheet(CSS_STYLE_FOR_TOP_HEADING)
         layout.addWidget(heading)
@@ -38,12 +49,24 @@ class RegistrationWindow(QWidget):
 
         btn_candidate = QPushButton(RegistrationWindow.BUTTON_TITLE_CANDIDATE_REGISTRATION)
         btn_candidate.setStyleSheet(CSS_STYLE_FOR_BUTTONS)
+        btn_candidate.clicked.connect( self.launch_candidate_registration )
 
         btn_voter = QPushButton(RegistrationWindow.BUTTON_TITLE_VOTER_REGISTRATION)
         btn_voter.setStyleSheet(CSS_STYLE_FOR_BUTTONS)
+        btn_voter.clicked.connect( self.launch_voter_registration )
 
         layout.addWidget(btn_candidate)
         layout.addSpacing(20)
         layout.addWidget(btn_voter)
 
         self.setLayout(layout)
+
+
+ 
+    def launch_candidate_registration( self ) :
+        self.candidate_registration.show()
+
+
+
+    def launch_voter_registration( self ) :
+        self.voter_registration.show()
