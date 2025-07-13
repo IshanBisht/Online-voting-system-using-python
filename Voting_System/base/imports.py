68e40ca0fa6f58ec7bf6f66aa393eba30a21b930
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-07-13 22:51:33 +0530

    Update imports.py

--- Voting_System/base/imports.py
+++ Voting_System/base/imports.py
@@ -1,8 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QComboBox, QCheckBox
-from PyQt5.QtGui import QIcon, QFont , QPixmap, QPalette, QBrush, QCloseEvent
+from PyQt5.QtGui import QIcon, QFont , QPixmap, QPalette, QBrush, QCloseEvent, QIntValidator
 from PyQt5.QtCore import Qt, QRect, QSize, QCoreApplication, QSize
 
-import pymysql
-
-from typing import Final
 from sys import argv , exit
+from hashlib import sha256
+import mysql.connector
