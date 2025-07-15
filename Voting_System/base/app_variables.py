4f1f111eb541f412165b9612deb2d35887ffbfc5
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-07-15 21:28:26 +0530

    Create app_variables.py

---
+++ Voting_System/base/app_variables.py
@@ -0,0 +1,15 @@
+PATH_TO_MAIN_PAGE_BACKGROUND_IMAGE = "Images/background.jpg"
+PATH_TO_ASHOKA_CHAKRA_IMAGE        = "Images/ashoka_chakra.png"
+PATH_TO_ELECTION_COMM_IMAGE        = "Images/election_commission.png"
+PATH_TO_APP_ICON                   = "Images/icon.png"
+COLOR_CODE_BACKGROUND              = "#567DD0"
+TITLE_MAIN_PAGE                    = "Online Voting System"
+
+TITLE_REGISTRATION_PAGE            = "Register to OVS"
+TITLE_CANDIDATE_REGISTRATION_PAGE  = "Candidate Registration"
+TITLE_VOTER_REGISTRATION_PAGE      = "Voter Registration"
+
+TITLE_LOGIN_PAGE                   = "Login to OVS"
+TITLE_ADMIN_LOGIN_PAGE             = "Admin Login"
+TITLE_VOTER_LOGIN_PAGE             = "Voter Login"
+TTILE_CANDIDATE_LOGIN_PAGE         = "Candidate Login"
