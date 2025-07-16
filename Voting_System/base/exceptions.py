bf86572bd9d3be121fb64b81600b4234ea9ba101
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-07-16 12:07:29 +0530

    Beautified

--- Voting_System/base/exceptions.py
+++ Voting_System/base/exceptions.py
@@ -1,15 +1,32 @@
-class OvsWrongLoginInfoException ( Exception ):
+class OvsException ( Exception ):
+    def __str__(self ) : return "OvsException"
 
+class OvsWrongLoginInfoException ( OvsException ):
     def __str__(self) : return "You have entered the wrong ID and Password!"
 
-class OvsInvalidCandidateIDException ( Exception ) :
 
+class OvsInvalidCandidateIDException ( OvsException ) :
     def __str__(self) : return "You have entered the invalid candidate ID!"
 
-class OvsInvalidAdminIDException ( Exception ) :
 
+class OvsInvalidAdminIDException ( OvsException ) :
     def __str__(self) : return "You have entered the invalid Admin ID!"
 
-class OvsInvalidVoterIDException ( Exception ) :
 
+class OvsInvalidVoterIDException ( OvsException ) :
     def __str__(self) : return "You have entered the invalid Voter ID!"
+
+
+class OvsCandidateAlreadyRegistered( OvsException ) :
+    def __str__(self) : return "The candidate is already registered before."
+
+
+class OvsVoterAlreadyRegistered ( OvsException ) :
+    def __str__(self) : return "The voter is already registered before."
+
+
+class OvsProgrammingError ( OvsException ):
+    def __str__(self) : return "Database connection is already closed before but it's methods are being used after closing."
+
+class OvsVoteAlreadyGivenException( OvsException ) :
+    def __str__(self) : return "You have already voted before."
