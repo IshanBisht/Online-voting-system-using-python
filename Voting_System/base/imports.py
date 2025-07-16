38b8c47be1395568b03811ac3a929995787c5e88
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-07-16 21:19:59 +0530

    Imported: QGridLayout

--- Voting_System/base/imports.py
+++ Voting_System/base/imports.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QComboBox, QCheckBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QComboBox, QCheckBox, QGridLayout
 from PyQt5.QtGui import QIcon, QFont , QPixmap, QPalette, QBrush, QCloseEvent, QIntValidator, QRegularExpressionValidator
 from PyQt5.QtCore import Qt, QRect, QSize, QCoreApplication, QSize, QRegularExpression
 
