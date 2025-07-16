29b169db6b1aa60a17ab8825ca059062582efd18
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-07-16 22:11:19 +0530

    Removed: unused modules

--- Voting_System/base/imports.py
+++ Voting_System/base/imports.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QComboBox, QCheckBox, QGridLayout
-from PyQt5.QtGui import QIcon, QFont , QPixmap, QPalette, QBrush, QCloseEvent, QIntValidator, QRegularExpressionValidator
-from PyQt5.QtCore import Qt, QRect, QSize, QCoreApplication, QSize, QRegularExpression
+from PyQt5.QtGui import QIcon, QFont , QPixmap, QCloseEvent, QIntValidator, QRegularExpressionValidator
+from PyQt5.QtCore import Qt, QRect, QSize, QSize, QRegularExpression
 
 from sys import argv , exit
 from hashlib import sha256
