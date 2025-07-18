ebee788e6c73a156ccb9f2d105e413588edfa5b1
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-07-18 19:01:39 +0530

    Saving names in capitalized | Added: new methods

--- Voting_System/base/data_manager.py
+++ Voting_System/base/data_manager.py
@@ -49,9 +49,9 @@ class DataManager:
         self.cursor.execute(f"""
             INSERT INTO { OVS_TABLE_CANDIDATE } VALUES(
                 {__aadhar_number},
-                '{__first_name}',
-                '{__last_name}',
-                '{__place}',
+                '{__first_name.capitalize()}',
+                '{__last_name.capitalize()}',
+                '{__place.lower()}',
                 '{DataManager.getHash( __password)}',
                 0,
                 0
@@ -91,12 +91,16 @@ class DataManager:
 
         if self.__isclosed : raise OvsProgrammingError()
 
+        self.cursor.execute(f"Select {OVS_COLUMN_AADHAR} from { OVS_TABLE_VOTER} where { OVS_COLUMN_AADHAR}= { __aadhar_number }");
+
+        if self.cursor.fetchone() : raise OvsVoterAlreadyRegistered()
+
         self.cursor.execute(f"""
             Insert into {OVS_TABLE_VOTER} values(
                  {__aadhar_number},
-                '{ __first_name}',
-                '{__last_name}',
-                '{__place}',
+                '{ __first_name.capitalize()}',
+                '{__last_name.capitalize()}',
+                '{__place.lower()}',
                 '{DataManager.getHash(__password)}',
                 0,
                 FALSE
@@ -108,10 +112,28 @@ class DataManager:
         self.cursor.execute(f"Select {OVS_COLUMN_ID} from {OVS_TABLE_VOTER} where {OVS_COLUMN_AADHAR}= {__aadhar_number}")
 
         return int(self.cursor.fetchone()[OVS_COLUMN_ID])
+    
+
+
+    def getAllCandidates( self , __place : str ) -> list[dict[int, int | str]] :
+
+        if self.__isclosed : raise OvsProgrammingError()
+
+        self.cursor.execute(f"SELECT * from { OVS_TABLE_CANDIDATE } where { OVS_COLUMN_PLACE }= '{ __place }'")
+
+        list_of_data = []
+
+        for row in self.cursor.fetchall() :
+            list_of_data.append({
+                KEY_ID : row[ OVS_COLUMN_ID ],
+                KEY_FULL_NAME : row[ OVS_COLUMN_FIRST_NAME] + ' ' + row[ OVS_COLUMN_LAST_NAME ]
+            })
+
+        return list_of_data
         
 
 
-    def getVoter(self, __voter_id: int, __password: str) -> dict[int, int | str]:
+    def getVoter(self, __voter_id: int, __password: str) -> dict[int, int | str | bool]:
 
         if self.__isclosed : raise OvsProgrammingError()
         
@@ -152,7 +174,7 @@ class DataManager:
         if self.__isclosed : raise OvsProgrammingError()
 
         # checking if vote already given by voter 
-        self.cursor.execute(f"Select {OVS_COLUMN_ID} from {OVS_TABLE_VOTER} where {OVS_COLUMN_ID}= {__voter_id}")
+        self.cursor.execute(f"Select {OVS_COLUMN_VOTE_GIVEN} from {OVS_TABLE_VOTER} where {OVS_COLUMN_ID}= {__voter_id}")
 
         row = self.cursor.fetchone()
 
@@ -195,7 +217,9 @@ class DataManager:
 
         row = self.cursor.fetchone()
 
-        if row : return {
+        if row is None : raise OvsNoPlaceFoundException()
+
+        return {
             KEY_ID          : row[OVS_COLUMN_ID],
             KEY_AADHAR_CARD : row[OVS_COLUMN_AADHAR],
             KEY_FIRST_NAME  : row[OVS_COLUMN_FIRST_NAME],
@@ -203,9 +227,6 @@ class DataManager:
             KEY_PLACE       : row[OVS_COLUMN_PLACE],
             KEY_VOTE_COUNT  : row[OVS_COLUMN_VOTE_COUNT]
         }
-
-        raise OvsNoPlaceFoundException()
-
 
 
     def getResult(self, __place: str) -> list[dict[int, int | str]]:
@@ -228,20 +249,74 @@ class DataManager:
         return results
     
 
+
+    def getAllResults( self ) -> list[dict[int, int | str]] :
+        
+        if self.__isclosed : raise OvsProgrammingError()
+
+        self.cursor.execute(f"SELECT * from { OVS_TABLE_RESULT } order by {OVS_COLUMN_PLACE}")
+
+        results = self.cursor.fetchall()
+
+        if results is None : raise OvsNoResultsDeclaredException()
+
+        output_list : list = []
+
+        for row in results :
+            output_list.append({
+                KEY_ID : row[ OVS_COLUMN_ID ],
+                KEY_FULL_NAME : row[ OVS_COLUMN_NAME ],
+                KEY_PLACE : row[ OVS_COLUMN_PLACE ],
+                KEY_VOTE_COUNT : row[ OVS_COLUMN_VOTE_COUNT]
+            })
+        
+        return output_list
+    
+
     def publishResult( self, __place : str ) -> None :
+
+        if self.__isclosed : raise OvsProgrammingError()
 
         data = self.getWinnerCandidate( __place )
 
         # writing the data to the ResultTable
-        self.cursor.execute(f"INSERT INTO {OVS_TABLE_RESULT} values( {data[KEY_PLACE]} , { str(data[KEY_FIRST_NAME]) + str(data[KEY_LAST_NAME]) }, { data[KEY_ID]} , { data[KEY_VOTE_COUNT]} )")
+        self.cursor.execute(f"INSERT INTO {OVS_TABLE_RESULT} values( '{data[KEY_PLACE]}' , '{ str(data[KEY_FIRST_NAME]) + str(data[KEY_LAST_NAME]) }', { data[KEY_ID]} , { data[KEY_VOTE_COUNT]} )")
 
         # now deleting all the candidates with same place
-        self.cursor.execute(f"delete * from {OVS_TABLE_CANDIDATE} where {OVS_COLUMN_PLACE}='{data[KEY_PLACE]}'")
+        self.cursor.execute(f"DELETE FROM {OVS_TABLE_CANDIDATE} where {OVS_COLUMN_PLACE}='{data[KEY_PLACE]}'")
 
         self.conn.commit()
 
-        
 
+
+    def getVoteCount( self, __candidate_id : int ) -> int : 
+
+        if self.__isclosed : raise OvsProgrammingError()
+
+        self.cursor.execute( f"SELECT { OVS_COLUMN_VOTE_COUNT } from { OVS_TABLE_CANDIDATE } where { OVS_COLUMN_ID}= { __candidate_id }" )
+
+        output = self.cursor.fetchone()
+
+        if ( output == None ) : raise OvsInvalidCandidateIDException()
+
+        return output[ OVS_COLUMN_VOTE_COUNT ]
+    
+
+
+    def raiseExceptionIfVoteAlreadyGiven( self , __voter_id : int ) -> None :
+
+        if self.__isclosed : raise OvsProgrammingError()
+
+        self.cursor.execute( f"SELECT { OVS_COLUMN_VOTE_GIVEN } from { OVS_TABLE_VOTER } where {OVS_COLUMN_ID}= { __voter_id }")
+
+        vote_given = self.cursor.fetchone()
+
+        if vote_given is None : raise OvsNoSuchColumnExistsException()
+
+        elif bool(vote_given[OVS_COLUMN_VOTE_GIVEN]) : raise OvsVoteAlreadyGivenException()
+
+
+        
     def close( self ) -> None :
 
         if self.__isclosed : return
